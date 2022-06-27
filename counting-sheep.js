cbce1924c7915c8019c3470152d4c27b355e1af5
Brian Hooks <bhooks00@gmail.com>
2022-06-26 20:23:59 -0700

    added new codewars challenge

--- counting-sheep.js
+++ counting-sheep.js
@@ -1,6 +1,4 @@
-/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
-For example,
+/* For example,
 
 [true,  true,  true,  false,
   true,  true,  true,  true ,
